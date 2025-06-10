f6ec6de7cca7035526391df809c730438614cc70
ktsim88 <526ktsim@frhsd.com>
2025-06-10 11:52:16 -0400

    started adding colored versions of products

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let firstName = document.getElementById('firstName').value
+// let lastName = document.getElementById('lastName').value
+// let email = document.getElementById('email').value
+// let phone = document.getElementById('phone').value
+// let country = document.getElementById('country').value
+// let comments = document.getElementById('comments').value
+
+// function submitContact() {
+//     firstName.value = ''
+//     lastName.value = ''
+//     email.value = ''
+//     phone.value = ''
+//     comments.value = ''
+// }
